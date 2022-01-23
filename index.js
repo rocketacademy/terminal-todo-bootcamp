d25cb36651d68c4787fbc85fbbac491ba775ae57
Amelia Wibisono <ameliawibi@gmail.com>
2022-01-23 13:54:11 +0800

    comfortable

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-console.log('hello world');
+import { add, write, read, edit } from "./jsonFileStorage.js";
+
+let action = process.argv[2];
+let userInput = process.argv[3];
+
+const handleJsonRead = (err, jsonContentObj) => {
+  const { items } = jsonContentObj;
+  if (action === "complete") {
+    if (
+      !err &&
+      userInput !== undefined &&
+      typeof (Number(userInput) === "number") &&
+      Number(userInput) > 0
+    ) {
+      const index = Number(userInput);
+      if (index > 0) {
+        items["Done"].push(items["Todo"][index - 1]);
+        items["Todo"].splice(index - 1, 1);
+      }
+    } else {
+      console.log(`Please input a number greater than 1`);
+    }
+  }
+  if (action === "add") {
+    if (!err && userInput !== undefined) {
+      console.log(`I have added ${userInput} to your to-do list`);
+    } else {
+      console.log(`Please input the item you wish to add`);
+    }
+  }
+  if (action === "show" || action === "complete") {
+    {
+      // If no error, edit the content
+      if (!err) {
+        console.log(`To do:`);
+        items["Todo"].forEach(function (value, i) {
+          console.log("%d: %s", i + 1, value);
+        });
+        console.log(`Done:`);
+        items["Done"].forEach(function (value, i) {
+          console.log("%d: %s", i + 1, value);
+        });
+      }
+    }
+  }
+};
+
+switch (action) {
+  case "show":
+    read("data.json", handleJsonRead);
+    break;
+  case "add":
+    add("data.json", "items", "Todo", userInput, handleJsonRead);
+    break;
+  case "complete":
+    edit("data.json", handleJsonRead, (err, jsonContentObj) => {
+      console.log("");
+    });
+}
